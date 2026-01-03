bdb243f585b208985ebdd94926937151539f2b44
jameschris854 <jameschris854@gmail.com>
2026-01-03 19:38:45 +0530

    glitch fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import { Routes, Route } from "react-router";
 import Home from "./pages/home";
 import CollectionsPage from "./pages/Collections";
+import { useLayoutEffect } from "react";
+
+
+const isLocalhost =
+  location.hostname === "localhost" ||
+  location.hostname === "127.0.0.1";
 
 export default function App() {
+
+  useLayoutEffect(() => {
+    // 🚨 Puppeteer runs on localhost → stay hidden
+    if (!isLocalhost) {
+      document.body.style.visibility = "visible";
+    }
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,6 @@ import { BrowserRouter } from 'react-router'
 import App from './App.tsx'
 import { HelmetProvider } from "react-helmet-async";
 
-const isLocalhost =
-  location.hostname === "localhost" ||
-  location.hostname === "127.0.0.1";
-
-if (!isLocalhost) {
-  // real users / production
-  requestAnimationFrame(() => {
-    document.body.classList.add("react-ready");
-  });
-}
 createRoot(document.getElementById('root')!).render(
    <HelmetProvider>
     <BrowserRouter>
